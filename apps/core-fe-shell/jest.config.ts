815986a658ec0aa4989c80ae32917d6323b0308d
Italo Amaral <italorockamaral@gmail.com>
2023-07-23 20:26:27 -0300

    config: rename project

--- apps/core-fe-shell/jest.config.ts
+++ apps/core-fe-shell/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'cliniccontrol',
+  displayName: 'core-fe-shell',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/cliniccontrol',
+  coverageDirectory: '../../coverage/apps/core-fe-shell',
 };
