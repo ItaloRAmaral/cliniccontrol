37dcd4153bbbb40128601af5ce49658abdfdc692
Italo Amaral <italorockamaral@gmail.com>
2023-07-29 16:12:59 -0300

    config: setting up swagger

--- apps/core-rest-api/src/main.ts
+++ apps/core-rest-api/src/main.ts
@@ -3,15 +3,41 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe, INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
+import { version } from "@clinicControl/root/package.json"
+
 
 import { AppModule } from './app/app.module';
+
+const setupOpenApi = (app: INestApplication) => {
+  // Setting up Swagger document
+  const options = new DocumentBuilder()
+    .setTitle('Clinic Controll Restful API')
+    .setDescription('Clinic Controll Restful API')
+    .setVersion(version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transformOptions: {
+        enableImplicitConversion: true, // With this option set to true, we no longer need to specify types with the @Type decorator;
+      },
+    })
+  );
+
+  // Setting up Swagger document
+  setupOpenApi(app);
 
   // Listen on specified port
   const port = process.env.PORT || 3000;
