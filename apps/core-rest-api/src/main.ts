f626d76e31af674fd9908348d62d24c2ba2b654e
luanavfg <luanavefago1@gmail.com>
2023-09-28 09:13:31 -0300

    config: config env module

--- apps/core-rest-api/src/main.ts
+++ apps/core-rest-api/src/main.ts
@@ -3,11 +3,12 @@
  * This is only a minimal backend to get started.
  */
 import { version } from '@clinicControl/root/package.json';
-import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
+import { INestApplication, Logger, ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { ApiModule } from '@clinicControl/core-rest-api/adapters/src/controllers/api/api.module';
+import { EnvService } from '@clinicControl/core-rest-api/adapters/src/env/env.service';
 
 const setupOpenApi = (app: INestApplication) => {
   // Setting up Swagger document
@@ -34,11 +35,16 @@ async function bootstrap() {
     })
   );
 
+  // Enable version
+  app.enableVersioning({type: VersioningType.URI})
+
   // Setting up Swagger document
   setupOpenApi(app);
 
   // Listen on specified port
-  const port = process.env.PORT || 3333;
+  const configService = app.get(EnvService)
+  const port = configService.get('PORT')
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
