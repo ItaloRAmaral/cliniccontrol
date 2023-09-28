79c60fdc743f8f1698adf53e5e9d6a2bc9e519d4
luanavfg <luanavefago1@gmail.com>
2023-09-28 09:42:48 -0300

    config: config jwt auth module

--- libs/core-rest-api/adapters/src/controllers/api/api.module.ts
+++ libs/core-rest-api/adapters/src/controllers/api/api.module.ts
@@ -1,12 +1,18 @@
 import { CreatePsychologistService } from '@clinicControl/core-rest-api/core/src/domains/psychologist/use-cases/create-psychologist/create-psychologist.service';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from '../../auth/auth.module';
 import { PostgreSqlPrismaOrmService } from '../../database/infra/prisma/prisma.service';
 import { DatabaseRepositoriesModule } from '../../database/repositories/repositories.module';
+import { envSchema } from '../../env/env';
 import { EnvModule } from '../../env/env.module';
 import { CreatePsychologistController } from './use-case/create-psychologist/create-psychologist.controller';
 
 @Module({
-  imports: [DatabaseRepositoriesModule, EnvModule],
+  imports: [DatabaseRepositoriesModule, EnvModule, AuthModule, ConfigModule.forRoot({
+    validate: (env) => envSchema.parse(env),
+    isGlobal: true
+  })],
   controllers: [CreatePsychologistController],
   providers: [PostgreSqlPrismaOrmService, CreatePsychologistService],
 })
