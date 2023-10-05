e0af32c2e4b875cff4f51d579686a2a7a953419a
luana-v-santos <luana.santos@dynamox.net>
2023-10-05 09:44:20 -0300

    feat(CC-69): add api guard

--- libs/core-rest-api/adapters/src/controllers/api/use-case/create-psychologist/create-psychologist.controller.ts
+++ libs/core-rest-api/adapters/src/controllers/api/use-case/create-psychologist/create-psychologist.controller.ts
@@ -1,12 +1,14 @@
 // eslint-disable-next-line @nx/enforce-module-boundaries
 import { PostgreSqlPrismaOrmService } from '@clinicControl/core-rest-api/adapters/src/database/infra/prisma/prisma.service';
 import { CreatePsychologistDto } from '@clinicControl/core-rest-api/core/src/domains/psychologist/use-cases/create-psychologist/create-psychologist-dto';
+// import { CreatePsychologistService } from '@clinicControl/core-rest-api/core/src/domains/psychologist/use-cases/create-psychologist/create-psychologist.service';
 import { CreatePsychologistService } from '@clinicControl/core-rest-api/core/src/domains/psychologist/use-cases/create-psychologist/create-psychologist.service';
 import { GlobalAppHttpException } from '@clinicControl/core-rest-api/core/src/shared/errors/globalAppHttpException';
 import { applicationValidateOrReject } from '@clinicControl/core-rest-api/core/src/shared/validators/validate-or-reject';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { plainToInstance } from 'class-transformer';
+import { ApiKeyGuard } from '../../guards/api-key.guard';
 
 @ApiTags()
 @Controller({
@@ -18,6 +20,7 @@ export class CreatePsychologistController {
     private createPsychologistService: CreatePsychologistService
   ) {}
   @Post('create')
+  @UseGuards(ApiKeyGuard)
   async execute(
     @Body() createPsychologistDto: CreatePsychologistDto
   ): Promise<null | undefined | void> {
---
+++ libs/core-rest-api/adapters/src/controllers/api/use-case/create-psychologist/nestjs-create-psychologist.service.ts
@@ -0,0 +1,5 @@
+import { CreatePsychologistService } from '@clinicControl/core-rest-api/core/src/domains/psychologist/use-cases/create-psychologist/create-psychologist.service';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class NestjsCreatePsychologistService extends CreatePsychologistService{}
