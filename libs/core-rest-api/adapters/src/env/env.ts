10de3ea7a681996e6895a139b9e755a244bfa488
Italo Amaral <italorockamaral@gmail.com>
2023-10-07 17:40:31 -0300

    config(CC-69): trying force nx cloud integration

--- libs/core-rest-api/adapters/src/env/env.ts
+++ libs/core-rest-api/adapters/src/env/env.ts
@@ -1,7 +1,7 @@
-import { z } from 'zod'
+import { z } from 'zod';
 
 export const envSchema = z.object({
-  // ##### Application ##### //
+  // ########## Application ##########
   // Environment
   NODE_ENV: z.string().default('development'),
   PORT: z.coerce.number().optional().default(3333),
@@ -10,13 +10,13 @@ export const envSchema = z.object({
   JWT_PRIVATE_KEY: z.string(),
   JWT_PUBLIC_KEY: z.string(),
 
-  // // ##### Database ##### //
+  // ########## Database ##########
   POSTGRES_USER: z.string(),
   POSTGRES_PASSWORD: z.string(),
   POSTGRES_DB: z.string(),
   POSTGRES_HOST: z.string(),
   POSTGRES_PORT: z.coerce.number().optional().default(5432),
-  POSTGRES_SCHEMA: z.string()
-})
+  POSTGRES_SCHEMA: z.string(),
+});
 
-export type Env = z.infer<typeof envSchema>
+export type Env = z.infer<typeof envSchema>;
