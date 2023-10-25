03edfd21e011d2e640fc44c5c40161c2bbbbb7eb
italo <italorockamaral@gmail.com>
2023-10-25 13:53:34 -0300

    config(CC-88): config setup bash scripts

---
+++ scripts/setup/core-rest-api/docker-compose-control.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Analisamos todos os argumentos fornecidos
+for arg in "$@"; do
+    if [[ $arg == --action=* ]]; then
+        # Extraímos o valor depois de '=' e substituímos as vírgulas por espaços
+        ACTIONS="${arg#*=}"
+        # Converta a string delimitada por vírgulas em um array
+        IFS=',' read -ra ACTIONS_ARRAY <<< "$ACTIONS"
+        shift
+    fi
+done
+
+# Verificamos e executamos as ações em ordem
+for action in "${ACTIONS_ARRAY[@]}"; do
+    if [ "$action" == "up" ]; then
+      docker compose -f "$PWD/docker-compose.yaml" up -d
+    elif [ "$action" == "down" ]; then
+      docker compose -f "$PWD/docker-compose.yaml" down -v
+    elif [ "$action" == "stop" ]; then
+      docker compose -f "$PWD/docker-compose.yaml" stop
+    elif [ "$action" == "restart" ]; then
+      docker compose -f "$PWD/docker-compose.yaml" restart
+    else
+        echo "Ação desconhecida: $action"
+    fi
+done
+
+echo "Ações executadas com sucesso."
---
+++ scripts/setup/core-rest-api/run-setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Subir o docker-compose
+echo "Subindo docker-compose..."
+docker compose -f "$PWD/docker-compose.yaml" up -d # rodar script sempre da raiz do projeto
+
+# Verificar se o docker-compose subiu corretamente
+if [ $? -ne 0 ]; then
+    echo "Erro ao subir o docker-compose."
+    exit 1
+fi
+
+# Dê algum tempo para que os serviços do docker-compose estejam prontos (se necessário)
+sleep 10
+
+# Rodar o script core-prisma-setup com os parâmetros passados
+echo "Executando core-prisma-setup..."
+./core-prisma-setup "$@"
+
+# Verificar se o core-prisma-setup rodou corretamente
+if [ $? -ne 0 ]; then
+    echo "Erro ao executar core-prisma-setup."
+    exit 1
+fi
+
+echo "Finalizado."
