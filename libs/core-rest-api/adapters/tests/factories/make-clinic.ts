56b7d4e54e8afb6b7864071f8c9acd865e69d4e3
Luana <luanavefago1@gmail.com>
2023-10-26 21:44:34 -0300

    feat(CC-81): make clinic factory

--- libs/core-rest-api/adapters/tests/factories/make-clinic.ts
+++ libs/core-rest-api/adapters/tests/factories/make-clinic.ts
@@ -3,21 +3,21 @@ import { Injectable } from '@nestjs/common';
 
 import { ClinicEntity } from '@clinicControl/core-rest-api/core/src/domains/clinic/entities/clinic/entity';
 import { CreateClinicDto } from '@clinicControl/core-rest-api/core/src/domains/clinic/use-cases/create-clinic/create-clinic-dto';
-import { randomUUID } from 'crypto';
 import { PostgreSqlPrismaOrmService } from '../../src/database/infra/prisma/prisma.service';
-import { PostgresqlPrismaPsychologistMapper } from '../../src/database/mappers/postgresql-prisma-psychologist-mapper';
+import { PostgresqlPrismaClinicMapper } from '../../src/database/mappers/postgresql-prisma-clinic-mapper';
 
 /*
   Creating a factory for the Clinic entity, which will be used to create tests for the domain's use cases
 */
 export function makeClinic(
+  psychologistId: string,
   override: Partial<CreateClinicDto> = {}
 ): ClinicEntity {
   const newClinic = new ClinicEntity({
     name: 'Nova Clinica',
     city: faker.location.city(),
     state: faker.location.state(),
-    psychologistId: randomUUID(), //TODO -> PSYCHOLOGIST ID
+    psychologistId: psychologistId,
     ...override,
   });
 
@@ -34,10 +34,10 @@ export class ClinicFactory {
   async makePrismaClinic(
     clinic: CreateClinicDto
   ): Promise<ClinicEntity> {
-    const newPrismaClinic = makeClinic(clinic);
+    const newPrismaClinic = makeClinic(clinic.psychologistId, clinic);
 
     await this.postgreSqlPrismaOrmService['clinic'].create(
-      PostgresqlPrismaPsychologistMapper.toPrismaCreate(newPrismaClinic)
+      PostgresqlPrismaClinicMapper.toPrismaCreate(newPrismaClinic)
     );
 
     return newPrismaClinic;
