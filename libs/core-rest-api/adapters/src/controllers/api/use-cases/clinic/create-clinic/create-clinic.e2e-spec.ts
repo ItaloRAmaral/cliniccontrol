acebf955d36e5855f9463d04ed4e8580a7443518
luanavfg <luanavefago1@gmail.com>
2023-11-08 08:21:48 -0300

    refactor(CC-81): change psychologist creation in clinic test to makePsychologist()

--- libs/core-rest-api/adapters/src/controllers/api/use-cases/clinic/create-clinic/create-clinic.e2e-spec.ts
+++ libs/core-rest-api/adapters/src/controllers/api/use-cases/clinic/create-clinic/create-clinic.e2e-spec.ts
@@ -3,10 +3,8 @@ import request from 'supertest';
 import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 
-import { PsychologistEntity } from '@clinicControl/core-rest-api/core/src/domains/psychologist/entities/psychologist/entity';
-import { Plan, Role } from '@clinicControl/core-rest-api/core/src/shared/interfaces/payments';
 import { makeClinic } from '@clinicControl/root/libs/core-rest-api/adapters/tests/factories/make-clinic';
-import { faker } from '@faker-js/faker';
+import { makePsychologist } from '@clinicControl/root/libs/core-rest-api/adapters/tests/factories/make-psychologist';
 import { PostgreSqlPrismaOrmService } from '../../../../../database/infra/prisma/prisma.service';
 import { ApiModule } from '../../../api.module';
 
@@ -27,14 +25,7 @@ describe('[E2E] - Create Clinic', () => {
   });
 
   it('[POST] - Should successfully create a new clinic', async () => {
-    const newPsychologist =
-    new PsychologistEntity({
-      name: 'Novo Usuário Teste',
-      email: 'novo_usuario_teste@gmail.com',
-      password: faker.internet.password({ length: 8 }),
-      plan: Plan.BASIC,
-      role: Role.ADMIN
-    })
+    const newPsychologist = makePsychologist();
 
     await request(app.getHttpServer())
       .post('/psychologist/create')
