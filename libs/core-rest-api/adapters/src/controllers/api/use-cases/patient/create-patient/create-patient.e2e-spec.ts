ac3b3d2a492d3c74a4ea5281864cb112a83f652f
Italo <italorockamaral@gmail.com>
2023-12-18 08:47:07 -0300

    test(CC-107): added e2e tests

--- libs/core-rest-api/adapters/src/controllers/api/use-cases/patient/create-patient/create-patient.e2e-spec.ts
+++ libs/core-rest-api/adapters/src/controllers/api/use-cases/patient/create-patient/create-patient.e2e-spec.ts
@@ -4,10 +4,12 @@ import request from 'supertest';
 import { CreatePatientDto } from '@clinicControl/core-rest-api/core/src/domains/patient/use-cases/create-patient/create-patient-dto';
 import { PaymentMethod } from '@clinicControl/core-rest-api/core/src/shared/interfaces/payments';
 import { INestApplication } from '@nestjs/common';
+import { PostgreSqlPrismaOrmService } from '../../../../../database/infra/prisma/prisma.service';
 import { setupE2ETest } from '../../../shared/utils/e2e-tests-initial-setup';
 
 describe('[E2E] - Create New Patient', () => {
   let app: INestApplication;
+  let prisma: PostgreSqlPrismaOrmService;
 
   let access_token: string;
 
@@ -18,6 +20,7 @@ describe('[E2E] - Create New Patient', () => {
     const setup = await setupE2ETest();
 
     app = setup.app;
+    prisma = setup.prisma;
 
     access_token = setup.access_token;
 
@@ -28,7 +31,7 @@ describe('[E2E] - Create New Patient', () => {
   it('[POST] - Should successfully create a new patient account', async () => {
     const newPatient: CreatePatientDto = {
       name: faker.person.fullName(),
-      email: faker.internet.email(),
+      email: "new_patient@email.com",
       cpf: faker.number.int({ min: 0, max: 10000000000 }).toString(),
       telephone: '+55 11 911111111',
       paymentMethod: PaymentMethod.CREDIT_CARD,
@@ -41,6 +44,70 @@ describe('[E2E] - Create New Patient', () => {
       .set('Authorization', `Bearer ${access_token}`)
       .send(newPatient);
 
+    const patientOnDatabase = await prisma.patient.findUnique({
+      where: {
+        email: newPatient.email,
+      },
+    })
+
     expect(response.statusCode).toBe(201);
+    expect(response.body.message).toBe('Patient created successfully');
+    expect(patientOnDatabase).toBeTruthy();
+  });
+
+  it('[POST] - Should return an error when trying to create a patient that already exists', async () => {
+    const newPatient: CreatePatientDto = {
+      name: faker.person.fullName(),
+      email: "new_patient@email.com",
+      cpf: faker.number.int({ min: 0, max: 10000000000 }).toString(),
+      telephone: '+55 11 911111111',
+      paymentMethod: PaymentMethod.CREDIT_CARD,
+      psychologistId: psychologistId,
+      clinicId: clinicId,
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/patient/create')
+      .set('Authorization', `Bearer ${access_token}`)
+      .send(newPatient);
+
+    expect(response.statusCode).toBe(409);
+    expect(response.body.message).toBe('patient already exists');
+  })
+
+  it('[POST] - Should return an error when trying to create a new patient without token', async () => {
+    const newPatient: CreatePatientDto = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      cpf: faker.number.int({ min: 0, max: 10000000000 }).toString(),
+      telephone: '+55 11 911111111',
+      paymentMethod: PaymentMethod.CREDIT_CARD,
+      psychologistId: psychologistId,
+      clinicId: clinicId,
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/patient/create')
+      .send(newPatient);
+
+    expect(response.statusCode).toBe(401);
+    expect(response.body.message).toBe('Invalid JWT token');
+  });
+
+  it('[POST] - Should return an error when trying to create a new patient without body request', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/patient/create')
+      .set('Authorization', `Bearer ${access_token}`);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).deep.equals([
+    'name must be a string',
+    'email must be a string',
+    'cpf must be a string',
+    'telephone must be a phone number',
+    'paymentMethod must be one of the following values: CREDIT_CARD, DEBIT_CARD, PIX, MONEY, HEALTH_INSURANCE, OTHER',
+    'psychologistId must be a string',
+    'clinicId must be a string'
+  ]);
   });
 });
