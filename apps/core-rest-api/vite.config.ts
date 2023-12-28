8734088d4dd099f3b55596288f9bc0fee997b975
luanavfg <luanavefago1@gmail.com>
2023-12-28 09:58:16 -0300

    chore(CC-113): adjust config files

---
+++ apps/core-rest-api/vite.config.ts
@@ -0,0 +1,33 @@
+/// <reference types='vitest' />
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+  cacheDir: '../../../node_modules/.vite/core-rest-api-core',
+
+  plugins: [
+    nxViteTsPaths(),
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+    tsconfigPaths(),
+  ],
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+
+  test: {
+    globals: true,
+    environment: 'node',
+    cache: {
+      dir: '../../../node_modules/.vitest',
+    },
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default'],
+  },
+});
---
+++ apps/core-rest-api/vitest.config.e2e.ts
@@ -0,0 +1,23 @@
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+  plugins: [
+    nxViteTsPaths(),
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+    tsconfigPaths(),
+  ],
+  test: {
+    globals: true,
+    include: ['**/*.e2e-spec.ts'],
+    root: './apps/core-rest-api/',
+    setupFiles: ['./tests/setup-e2e.ts'],
+    environment: 'node',
+    reporters: ['default'],
+  },
+});
