8ae8d814810600f729969a107ed0e1cf85cc3edc
luanavfg <luanavefago1@gmail.com>
2024-01-16 08:42:44 -0300

    feat: add appointment mapper

---
+++ apps/core-rest-api/src/app/adapters/database/mappers/postgresql-prisma-appointment-mapper.ts
@@ -0,0 +1,40 @@
+import {
+  Prisma,
+  Appointment as PrismaAppointmentDto,
+  PaymentMethod as PrismaPaymentMethod,
+} from '@prisma/client';
+
+import { AppointmentEntity } from '../../../core/domains/appointment/entities/appointment/entity';
+import { CreateSingleAppointmentDto } from '../../../core/domains/appointment/use-cases/create-single-appointment/create-single-appointment-dto';
+import { UpdateAppointmentInfoDto } from '../../../core/domains/appointment/use-cases/update-appointment-info/update-appointment-info-dto';
+import { PaymentMethod } from '../../../core/shared/interfaces/payments';
+
+export class PostgresqlPrismaAppointmentMapper {
+  static toDomain(raw: PrismaAppointmentDto): AppointmentEntity {
+    return new AppointmentEntity({
+      ...raw,
+      paymentMethod: raw.paymentMethod as unknown as PaymentMethod,
+    });
+  }
+
+  static toPrismaCreate(raw: CreateSingleAppointmentDto): Prisma.AppointmentCreateArgs {
+    return {
+      data: {
+        ...raw,
+        paymentMethod: raw.paymentMethod as unknown as PrismaPaymentMethod,
+      },
+    };
+  }
+
+  static toPrismaUpdate(raw: UpdateAppointmentInfoDto): Prisma.AppointmentUpdateArgs {
+    return {
+      data: {
+        ...raw,
+        done: raw.done as boolean
+      },
+      where: {
+        id: raw.id,
+      },
+    };
+  }
+}
