6f92fd09b55583867c42b50e3229d831e68b3665
luana-v-santos <luana.santos@dynamox.net>
2024-04-25 09:24:08 -0300

    fix(CC-119): fix controller output of appointment

--- apps/core-rest-api/src/app/adapters/routes/v1/appointment/update-appointment/output.dto.ts
+++ apps/core-rest-api/src/app/adapters/routes/v1/appointment/update-appointment/output.dto.ts
@@ -1,55 +1,7 @@
-import { PaymentMethod } from '@prisma/client';
-import { IsBoolean, IsDate, IsEnum, IsObject, IsString } from 'class-validator';
+import { IsObject, IsString } from 'class-validator';
+import { AppointmentEntity } from '../../../../../core/domains/appointment/entities/appointment/entity';
 
-class UpdatedAppointmentInfoOutputDto {
-  @IsString()
-  id!: string;
-
-  @IsString()
-  psychologistId!: string;
-
-  @IsString()
-  patientId!: string;
-
-  @IsDate()
-  date!: Date;
-
-  @IsBoolean()
-  online!: boolean;
-
-  @IsString()
-  clinicId!: string;
-
-  @IsBoolean()
-  confirmed!: boolean;
-
-  @IsDate()
-  confirmationDate?: Date | null;
-
-  @IsBoolean()
-  cancelled!: boolean;
-
-  @IsDate()
-  cancellationDate?: Date | null;
-
-  @IsBoolean()
-  done!: boolean | null;
-
-  @IsBoolean()
-  missed?: boolean | null;
-
-  @IsBoolean()
-  paid?: boolean;
-
-  @IsEnum(PaymentMethod)
-  paymentMethod!: PaymentMethod;
-
-  @IsDate()
-  createdAt!: Date;
-
-  @IsDate()
-  updatedAt?: Date | null;
-}
+class UpdatedAppointmentInfoOutputDto extends AppointmentEntity{}
 
 export class UpdateAppointmentControllerOutputDto {
   @IsString()
